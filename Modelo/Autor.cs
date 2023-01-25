1686f7f38f4f5b94e4d72c1c6d5ad67e4e9145e6
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-01-25 11:29:26 +0100

    Modelo hecho

---
+++ Modelo/Autor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorRestReview.Modelo
+{
+    class Autor
+    {
+        private int id;
+        private String nombre;
+        private String imagen;
+        private String nickName;
+    }
+}
---
+++ Modelo/Seccion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorRestReview.Modelo
+{
+    class Seccion
+    {
+        private int id;
+        private int nombre;
+    }
+}
