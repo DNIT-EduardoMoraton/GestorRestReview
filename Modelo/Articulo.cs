5a7bdfd6c17efbff3257cec0e11c93ed0353d386
Jose Antonio Guerrero Vidal <ja.guerrero@iesdoctorbalmis.com>
2023-01-25 12:33:19 +0100

    autorizo

--- Modelo/Articulo.cs
+++ Modelo/Articulo.cs
@@ -6,13 +6,61 @@ using System.Threading.Tasks;
 
 namespace GestorRestReview.Modelo
 {
-    class Articulo
+    public class Articulo
     {
         private int id;
         private int idAutor;
         private int idSeccion;
-        private String texto;
-        private String titulo;
-        private String imagen;
+        private string texto;
+        private string titulo;
+        private string imagen;
+
+        // Constructor
+        public Articulo(int id, int idAutor, int idSeccion, string texto, string titulo, string imagen)
+        {
+            this.id = id;
+            this.idAutor = idAutor;
+            this.idSeccion = idSeccion;
+            this.texto = texto;
+            this.titulo = titulo;
+            this.imagen = imagen;
+        }
+
+        // Getters y Setters
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int IdAutor
+        {
+            get { return idAutor; }
+            set { idAutor = value; }
+        }
+
+        public int IdSeccion
+        {
+            get { return idSeccion; }
+            set { idSeccion = value; }
+        }
+
+        public string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public string Imagen
+        {
+            get { return imagen; }
+            set { imagen = value; }
+        }
     }
 }
