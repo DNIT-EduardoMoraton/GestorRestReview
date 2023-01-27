56dc62297bf3258027f49cbe3833e23ecb5cb256
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-01-27 13:47:29 +0100

    hecho avances

--- Vistas/UserControls/Articulos/ArticulosUserControlVM.cs
+++ Vistas/UserControls/Articulos/ArticulosUserControlVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestorRestReview.Servicios;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,13 +9,35 @@ using System.Windows.Controls;
 
 namespace GestorRestReview.Vistas.UserControls.Articulos
 {
-
-
-    //
-    class ArticulosUserControlVM
+    class ArticulosUserControlVM : ObservableRecipient
     {
+        private UserControl currUserControl;
+
+        public UserControl CurrUserControl
+        {
+            get { return currUserControl; }
+            set { SetProperty(ref currUserControl, value); }
+        }
+
+        // Commands
 
 
 
+        // Services
+
+        private NavegacionServicio servicioNavegacion;
+
+
+        public ArticulosUserControlVM()
+        {
+            servicioNavegacion = new NavegacionServicio();
+            InicioPorDefecto();
+            
+        }
+
+        private void InicioPorDefecto()
+        {
+            CurrUserControl = servicioNavegacion.irArticulosListaUserControl();
+        }
     }
 }
