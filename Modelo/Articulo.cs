a0e2460552c576e010f29766ede39fe975877e86
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-01-30 14:07:38 +0100

    Avances en gestionar

--- Modelo/Articulo.cs
+++ Modelo/Articulo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,69 +7,13 @@ using System.Threading.Tasks;
 
 namespace GestorRestReview.Modelo
 {
-    public class Articulo
+    class Articulo : ObservableObject
     {
         private int id;
         private int idAutor;
         private int idSeccion;
-        private string texto;
-        private string titulo;
-        private string imagen;
-
-        public Articulo()
-        {
-        }
-
-
-
-        // Constructor
-        public Articulo(int id, int idAutor, int idSeccion, string texto, string titulo, string imagen)
-        {
-            this.id = id;
-            this.idAutor = idAutor;
-            this.idSeccion = idSeccion;
-            this.texto = texto;
-            this.titulo = titulo;
-            this.imagen = imagen;
-        }
-
-        // Getters y Setters
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        public int IdAutor
-        {
-            get { return idAutor; }
-            set { idAutor = value; }
-        }
-
-        public int IdSeccion
-        {
-            get { return idSeccion; }
-            set { idSeccion = value; }
-        }
-
-        public string Texto
-        {
-            get { return texto; }
-            set { texto = value; }
-        }
-
-        public string Titulo
-        {
-            get { return titulo; }
-            set { titulo = value; }
-        }
-
-        public string Imagen
-        {
-            get { return imagen; }
-            set { imagen = value; }
-        }
-
-
+        private String texto;
+        private String titulo;
+        private String imagen;
     }
 }
