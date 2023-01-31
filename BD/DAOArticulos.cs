6909298c41dbda8690101178dfee88510026ca6c
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-01-31 14:54:13 +0100

    UPDATE DE DEPENDENCIAS

--- BD/DAOArticulos.cs
+++ BD/DAOArticulos.cs
@@ -1,10 +1,6 @@
 ﻿using GestorRestReview.Modelo;
 using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GestorRestReview
 {
